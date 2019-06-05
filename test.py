ff237443f13950b4eee19b7c588e987b49e3d893
sgalvan <sakura-lienzo@hotmail.com>
2019-06-04 23:46:31 -0500

    Is this my test defining functions y python

---
+++ test.py
@@ -0,0 +1,13 @@
+##!/bin/python3
+print(3+2)
+a = 2 ** 1024
+print(a)
+ 
+def product_integers(x, y, z, k):
+   return x*y*z*k
+
+def sum_any_number(x, y, z):
+   return x + y + x
+
+def subs_any_number(x, y):
+   return x - y
