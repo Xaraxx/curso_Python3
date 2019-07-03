e55bf817fa721b0474550ca36877159ec3b9efb2
sgalvan <sakura-lienzo@hotmail.com>
2019-07-02 19:39:21 -0500

    This files are from the new course python basics

---
+++ basics/turtle.py
@@ -0,0 +1,14 @@
+import turtle 
+
+window = turtle.Screen()
+dav = turtle.Turtle()
+dav.forward(50)
+dav.left(90)
+dav.forward(50)
+dav.left(90)
+dav.forward(50)
+dav.left(90)
+dav.forward(50)
+dav.left(90)
+
+turtle.mainloop()
